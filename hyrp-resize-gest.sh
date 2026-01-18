14b1194969d351308a0b84f40627a0d9253fef98
Maksym Kapula <maksym.kapula@gmail.com>
2026-01-18 22:55:23 +0200

    18-01-26

---
+++ hyrp-resize-gest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# приклад через ydotool (емуляція Super+RightMouse)
+ydotool key 125:1             # Super down
+ydotool click 272             # RMB down/up
+ydotool key 125:0             # Super up
